1f96b6c72659cab84cbe23f5dd7da98e92cd6453
Mayank2112 <30808mayank21@gmail.com>
2024-02-29 10:42:09 +0530

    connect to the database

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
 
+mongoose.connect('mongodb+srv://mayank:mayank2112@soical-media.wuiqgz6.mongodb.net/social_media?retryWrites=true&w=majority&appName=soical-media').then(
+    ()=>{console.log('Database is connected!');}).catch(
+        (err)=>{console.log(err)}
+    );
 const app = express();
 
 app.listen(3000,()=>{
